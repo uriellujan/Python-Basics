61bd2519a26d91912e792bd7f0ca50423df4df1b
stealthcamaro <lujanuriel@icloud.com>
2026-03-28 20:58:06 -0500

    Created a file where I demonstrate my ability to create and use variables in formatted strings.

--- module1/variables.py
+++ module1/variables.py
@@ -1,8 +1,24 @@
-# This is probably NOT my first Python program
-print("I like tacos!")
-print("It's really good!")
+# This is module1.py where the focus is to demonstrate my ability
+# to work with basic/core Python concepts such as variables,
+# dynamic typing, and standard operators.
 
+# String
+first_name = "Uriel"
+food = "tacos"
+email = "uriel123@fakemail.net"
+print(f"Hello {first_name}\nYou like to eat {food}\nYour email is NOT {email}")
 
-first_name = "Ceedee"
+# Integer
+age = 21
+quantity = 3
+print(f"You are NOT {age} years old.\nYou have {quantity} bananas.")
 
-print(f"Hello {first_name}")
+# Float
+weight = 6.7
+miles = .25
+print(f"I weigh more than {weight} kilograms.\nA drag strip is usually {miles} miles long.")
+
+# Boolean
+is_a_superhero = True
+is_a_supervillan = False
+print(f"Is Iron Man a superhero? {is_a_superhero}\nIs Thanos a supervillan? {is_a_supervillan}")
